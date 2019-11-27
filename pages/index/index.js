08d968db51845d0061d33643bb1ffaba764033c7
CARRY-weng <53970982+CARRY-weng@users.noreply.github.com>
2019-11-27 13:11:43 +0800

    轮播图雏形完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,19 @@
 
 Page({
+    data:{
+        swiperList:[]
+    },
+    onLoad(){
+        wx.request({
+            url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+            success: (result) => {
+                console.log(result.data.message);
+                this.setData({
+                    swiperList:result.data.message
+                })
+                
+            }
+        });
+          
+    }
 })
