6cec9dea406bb2381b12e4458f278d48bf3ebf05
CARRY-weng <53970982+CARRY-weng@users.noreply.github.com>
2019-11-27 15:25:28 +0800

    首页导航栏静态完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,11 @@
 
 Page({
     data:{
-        swiperList:[]
+        swiperList:[],  //轮播图数据
+        navigationList:[]  //导航栏数据
     },
     onLoad(){
+        //获取轮播图的请求
         wx.request({
             url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
             success: (result) => {
@@ -14,6 +16,28 @@ Page({
                 
             }
         });
-          
+
+        //获取导航数据请求
+        wx.request({
+             url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+             success: (result) => {
+                //  console.log(result.data.message);
+                 this.setData({
+                    navigationList:result.data.message
+                 })
+                 
+             }
+         });
+
+        //  //获取楼层数据请求
+        //  wx.request({
+        //      url: 'https://api.zbztb.cn/api/public/v1/home/floordata',
+        //      success: (result) => {
+        //          console.log(result);
+                 
+        //      }
+        //  });
+           
+            
     }
 })
