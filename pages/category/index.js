4e2da4a2fd130c54568ec475d47d01bc89352e42
CARRY-weng <53970982+CARRY-weng@users.noreply.github.com>
2019-11-29 15:37:30 +0800

    第一次渲染分页数据

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,3 +1,5 @@
+import request from '../../utils/request'
+
 // pages/category/index.js
 Page({
 
@@ -5,14 +7,44 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    catList:[],  //存放侧边栏的数据
+    chilireList:[],  //存放右边的数据
+    catIndex:0
+  },
+  
+  Cats:[],  //全局数据 页面是拿不到的 只是用来存放全部数据 为了然页面不卡
+  handletap(e){
+    // console.log(e);
+    this.setData({
+      catIndex:e.currentTarget.dataset.index
+    })
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // wx.request({
+    //   url: 'https://api.zbztb.cn/api/public/v1/categories',
+    //   success: (result) => {
+    //     console.log(result.data.message);
+    //     this.setData({
+    //       catList:result.data.message
+    //     })
+    //   }
+    // });
+    request({url:'categories'})
+      .then(result=>{
+        this.Cats=result.data.message, //先把所有数据存在data外面的全局数据中 
+        this.setData({
+          //给左侧菜单栏赋值
+          catList:this.Cats.map(v=>v.cat_name),
+          //右边的内容
+          chilireList:this.Cats[0].children
+        })
+      })
+      
   },
 
   /**
