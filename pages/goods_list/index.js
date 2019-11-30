152e15112d2305487bd0a1db0277150c6cc6ec8d
CARRY-weng <53970982+CARRY-weng@users.noreply.github.com>
2019-11-30 19:15:19 +0800

    商品列表页分页

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -7,7 +7,7 @@ Page({
     query:'',   //搜索关键字
     cid:-1,     //分类id
     pagenum:1,  //页码
-    pagesize:2  //也容量
+    pagesize:10  //页容量
   },
   /**
    * 页面的初始数据
@@ -21,26 +21,61 @@ Page({
    */
   onLoad(option) {
     // console.log(option);
-    this.Params.cid = option.cid;
+    this.Params.cid = option.cid;  //这个option是在url后面的参数 onLoad事件可以获取到
     this.getGoods()
     
   },
+  //总页数
+  TotalPages :1,
 
+  //发送请求的方法
   getGoods(){
     request({
       url:'goods/search',
       data:this.Params
     }).then(res=>{
-      console.log(res.data.message.goods);
+      // console.log(res.data.message.goods);
+      const {goods} = this.data  //为了上滑加载数据的时候是把新的数据拼接在旧数据里面
       this.setData({
-        goods:res.data.message.goods
+         // 当我们做分页了  总的数据 应该是不断 追加的！！！ 
+        goods:[...goods, ...res.data.message.goods]
       })
+      // console.log(res.data.message);  总条数就在 res.data.message.total
+      
+      //计算总页数
+      this.TotalPages=Math.ceil(res.data.message.total / this.Params.pagesize )
+      // console.log(this.TotalPages);
+      
     })
   },
 
+  //滚动条触底时候触发的函数
   onReachBottom(){
-    console.log('aaaa');
+    // console.log('aaaa');
+    //判断有没有下一页数据 
+    if(this.Params.pagenum >= this.TotalPages){  //没有页码数大于等于总条数 就是没有下一页了
+      //提示用户
+      wx.showToast({
+        title: '没有更多',
+        icon: 'success',
+        duration: 1500,
+        mask: true
+      });
+        
+    }else{  //还有下一页数据 继续发送请求 并把发送的页数加一
+      this.Params.pagenum++;
+      this.getGoods()
+    }
     
-  }
+  },
+
+  // //顶部下拉事件
+  // onPullDownRefresh(){
+  //   console.log('bbbbbbbbb');
+    
+  // }
  
 })
+
+
+  
