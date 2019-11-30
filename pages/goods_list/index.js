785da79012e70a583a5ee626a5ab037b472a7720
CARRY-weng <53970982+CARRY-weng@users.noreply.github.com>
2019-11-30 14:22:39 +0800

    商品内容动态展示

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,66 +1,46 @@
+import request from '../../utils/request'
+
 // pages/goods_list/index.js
 Page({
-
+  // 全局参数
+  Params:{
+    query:'',   //搜索关键字
+    cid:-1,     //分类id
+    pagenum:1,  //页码
+    pagesize:2  //也容量
+  },
   /**
    * 页面的初始数据
    */
   data: {
-
+    goods:[]  //商品列表
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad(option) {
+    // console.log(option);
+    this.Params.cid = option.cid;
+    this.getGoods()
+    
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getGoods(){
+    request({
+      url:'goods/search',
+      data:this.Params
+    }).then(res=>{
+      console.log(res.data.message.goods);
+      this.setData({
+        goods:res.data.message.goods
+      })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onReachBottom(){
+    console.log('aaaa');
+    
   }
+ 
 })
